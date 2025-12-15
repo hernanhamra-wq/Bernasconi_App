c43d59eb44be2e4b12cb431b288b5eb9f95c3037
NurAr <110198601+NuriArn@users.noreply.github.com>
2025-12-15 01:33:09 -0300

    carga ficha tecnica

--- bernasconi_app/apps/ficha_tecnica/forms.py
+++ bernasconi_app/apps/ficha_tecnica/forms.py
@@ -1,14 +1,27 @@
 from django import forms
 from .models import FichaTecnica
+from apps.material.models import Material
+
 
 class FichaTecnicaForm(forms.ModelForm):
+
+    # ✅ Materiales como checklist (en vez de select chiquito)
+    materiales = forms.ModelMultipleChoiceField(
+        queryset=Material.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Materiales"
+    )
+
     class Meta:
         model = FichaTecnica
         fields = '__all__'
+
         labels = {
             'n_de_ficha': 'N° de Ficha',
             'inventario': 'Inventario',
             'n_de_inventario_anterior': 'N° Inventario anterior',
+            'titulo': 'Título',
             'anio': 'Año',
             'dimensiones': 'Dimensiones (texto)',
             'fk_estado': 'Estado de la obra',
@@ -16,9 +29,12 @@ class FichaTecnicaForm(forms.ModelForm):
             'fk_serie': 'Serie',
             'fk_multimedia_principal': 'Imagen principal',
             'seguimiento': 'Requiere seguimiento',
+            'descripcion': 'Descripción',
+            'observacion': 'Observación',
         }
 
         widgets = {
+            'n_de_inventario_anterior': forms.TextInput(),
             'descripcion': forms.Textarea(attrs={'rows': 3}),
             'observacion': forms.Textarea(attrs={'rows': 3}),
             'dimensiones': forms.Textarea(attrs={'rows': 2}),
